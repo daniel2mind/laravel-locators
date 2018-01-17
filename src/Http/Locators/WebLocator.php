4b48988ac8659510e8df1a5fb479eb91222e66bc
Daniel Barcelos <daniel.barcelos@2mind.com.br>
2018-01-17 10:05:58 -0200

    Update WebLocator.php

--- src/Http/Locators/WebLocator.php
+++ src/Http/Locators/WebLocator.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
- *  WebController possui as funções que serão instanciadas pelas rotas web ('routes/web.php'), onde cada função irá realizar
- *  a chamada na respectiva controller e tratar o retorno vindo das controllers
- *
- *  A intenção de estruturar as chamadas dessa maneira, é para que as Controllers apenas executem suas regras de negócio,
- *  deixando que a WebController redirecione para os dados para as páginas ou API (em caso de chamada via ApiController)
- *
- *
- *  Por Daniel Barcelos
- *  12/07/2017 - 11:09
- *
- */
-
-
 namespace App\Http\Locators;
 
 use App\Http\Controllers\LocatorExampleController;
